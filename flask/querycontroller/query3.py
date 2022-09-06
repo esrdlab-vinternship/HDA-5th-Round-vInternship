8a7c9b32434a257d1c43fbcab3466b26237d56aa
SadiaAlamChy <sadiaalamsaki@gmail.com>
2022-09-07 00:55:26 +0600

    task5

--- flask/querycontroller/query3.py
+++ flask/querycontroller/query3.py
@@ -1,5 +1,7 @@
 from dbconnection.dbcon import PostgresConnection
 import pandas as pd
+
+
 class Query3:
     def __init__(self):
         self.con = PostgresConnection().getConnection()
@@ -9,12 +11,12 @@ class Query3:
         con = PostgresConnection().getConnection()
         cur = con.cursor()
         query3 = "SELECT s.division, SUM(t.total_price) " \
-              "FROM ecomdb_star_schema.fact_table t " \
-              "JOIN ecomdb_star_schema.store_dim s on s.store_key=t.store_key " \
-                "JOIN ecomdb_star_schema.time_dim tim on tim.time_key=t.time_key " \
-                "WHERE s.division='BARISAL' "\
-                "GROUP BY CUBE(s.division) "\
-                "ORDER BY s.division"
+                 "FROM ecomdb_star_schema.fact_table t " \
+                 "JOIN ecomdb_star_schema.store_dim s on s.store_key=t.store_key " \
+                 "JOIN ecomdb_star_schema.time_dim tim on tim.time_key=t.time_key " \
+                 "WHERE s.division='BARISAL' " \
+                 "GROUP BY CUBE(s.division) " \
+                 "ORDER BY s.division"
         cur.execute(query3)
         result = cur.fetchall()
         pd_data = pd.DataFrame(list(result), columns=['division', 'total_sale_price'])
@@ -22,6 +24,7 @@ class Query3:
         pd_data = pd_data.dropna()
         # print(pd_data)
         return pd_data.to_dict(orient='records')
+
 
 if __name__ == '__main__':
     q3 = Query3()
